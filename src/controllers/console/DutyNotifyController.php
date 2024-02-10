49c53529b97880f60bca4a7133f6cdf2d991af69
dev8 <dev8.sofona@gmail.com>
2024-02-10 16:44:06 +0200

    +notify & add message

--- src/controllers/console/DutyNotifyController.php
+++ src/controllers/console/DutyNotifyController.php
@@ -2,11 +2,8 @@
 
 namespace app\reminder\controllers\console;
 
-use app\reminder\config\TelegramBotDto;
 use app\reminder\helpers\Helper;
 use app\reminder\repository\DutyLine;
-use app\toolkit\services\SettingsService;
-use TelegramBot\Api\BotApi;
 
 
 class DutyNotifyController implements \app\toolkit\components\controllers\ConsoleControllerInterface
@@ -17,20 +14,6 @@ class DutyNotifyController implements \app\toolkit\components\controllers\Consol
             return;
         }
 
-        $current = (new DutyLine)->getCurrent();
-
-        /** @var TelegramBotDto $options */
-        $options = SettingsService::load('telegram', TelegramBotDto::class);
-
-        if ($current) {
-            $chatId = $options->chatId;
-            $message[] = 'Сегодня дежурен ' . $current[0];
-            $message[] = 'День дежурста ' . (empty($current[0]) ? 'первый' : 'второй');
-        } else {
-            $chatId = $options->adminChatId;
-            $message[] = 'Ошибка';
-        }
-
-        (new BotApi($options->token))->sendMessage($chatId, join(PHP_EOL, $message));
+        Helper::notify((new DutyLine));
     }
 }
--- src/helpers/Helper.php
+++ src/helpers/Helper.php
@@ -3,12 +3,38 @@
 namespace app\reminder\helpers;
 
 
+use app\reminder\config\TelegramBotDto;
+use app\toolkit\services\SettingsService;
+use TelegramBot\Api\BotApi;
+
 class Helper
 {
     const WORK_DAYS = [1, 2, 3, 4, 5];
 
     public static function currentDayIsWorkDay(): bool
     {
+        return true;
         return in_array(date('w'), self::WORK_DAYS);
+    }
+
+
+    public static function notify($line)
+    {
+        $current = $line->getCurrent();
+
+        /** @var TelegramBotDto $options */
+        $options = SettingsService::load('telegram', TelegramBotDto::class);
+
+        if ($current) {
+            $chatId = $options->chatId;
+            $message[] = 'Сегодня дежурен ' . $current[0];
+            $message[] = 'День дежурста ' . (empty($current[0]) ? 'первый' : 'второй');
+            $message[] = "\nПожалуйста, не забывайте выносить мусор!";
+        } else {
+            $chatId = $options->adminChatId;
+            $message[] = 'Ошибка';
+        }
+
+        (new BotApi($options->token))->sendMessage($chatId, join(PHP_EOL, $message));
     }
 }
